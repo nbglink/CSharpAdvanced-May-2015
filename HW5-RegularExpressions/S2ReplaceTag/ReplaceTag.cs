7ec3b88922c2351c26aebbf83bc67fd5d19407a5
nbglink@gmail.com <nbglink@gmail.com>
2015-05-17 22:51:24 +0300

    RegularExpressions

---
+++ HW5-RegularExpressions/S2ReplaceTag/ReplaceTag.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace S2ReplaceTag
+{
+    class ReplaceTag
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            string pattern = @"<a\s+href=([^>]+)>([^<]+)</a>";
+            Regex regex = new Regex(pattern);
+            string replacement = "[URL href=$1]$2[/URL]";
+            string result = Regex.Replace(input, pattern, replacement);
+            Console.WriteLine(result);
+        }
+    }
+}
